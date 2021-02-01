dd10c23223e83d24ed814ed6ee040bcc273e4a70
MariaZarifyan <masha.zarifyan@gmail.com>
2021-02-02 00:17:33 +0100

    added responsiveness

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ const navbarLinks = document.getElementsByClassName("nav-links")[0]
 
 
 toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active")
+    navbarLinks.classList.toggle("visible")
 })
+
+
