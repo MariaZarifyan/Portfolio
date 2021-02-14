9a0a6d73eb2db5a27a7d2a75d3a85a45e6d85a47
MariaZarifyan <masha.zarifyan@gmail.com>
2021-02-14 14:47:43 +0100

    added animation

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+
+window.addEventListener("load", () => {
+    document.querySelector("body").classList.add("loaded"); 
+   });
+
+$(function(){  // $(document).ready shorthand
+    $('.monster').fadeIn('slow');
+  });
+  
+  $(document).ready(function() {
+      
+      $(window).scroll( function(){
+      
+
+          $('.hideme').each( function(i){
+              
+              var bottom_of_object = $(this).position().top + $(this).outerHeight()/3;
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+              if( bottom_of_window > bottom_of_object ){
+                  
+                  $(this).animate({'opacity':'1'},800);
+                      
+              }
+              
+          }); 
+      
+      });
+      
+  });
+
+
+
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navbarLinks = document.getElementsByClassName("nav-links")[0]
 
